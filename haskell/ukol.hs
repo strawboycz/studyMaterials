6174b54bd900bf05d46887ba0ba696e6bb6dedf2
Miroslav Přeček <99765327+strawboycz@users.noreply.github.com>
2024-12-03 08:46:34 +0100

    Update ukol.hs

--- haskell/ukol.hs
+++ haskell/ukol.hs
@@ -92,6 +92,28 @@ putIntoMaze maze ((x,y,char):triplets) = let
 getPart :: Maze -> (Int,Int) -> (Int,Int) -> Maze
 getPart maze (x,y) (height,width) = take height (drop x [take width (drop y row) | row <- maze])
 
-solveMaze :: Maze -> Int
-solveMaze maze = let
+getNeighbours :: (Num a, Num b) => (a, b) -> [(a, b)]
+getNeighbours (r1, c1) = [(r1 - 1, c1), (r1 + 1, c1), (r1, c1 - 1), (r1, c1 + 1)]
+
+solve :: (Num n, Num a, Num b, Eq a, Eq b) => [(a, b, n)] -> [(a, b)] -> n
+solve [] _ = 0
+solve ((r1, c1, price):toSolve) nowEmpty =
+    let
+        allNeighbours = [n | n <- getNeighbours (r1, c1), elem n nowEmpty]
+        newEmpty = nowEmpty \\ allNeighbours
+    in (r1, c1, price) :
+       solve (toSolve ++ [(r, c, price + 1) | (r, c) <- allNeighbours]) newEmpty
+
+solveMaze :: Num a => [[Char]] -> Int
+solveMaze maze =
+    let
+        indexes = concat [[(ri, ci, ch) | (ci, ch) <- zip [0..] line] | (ri, line) <- zip [0..] maze]
+        (sr, sc) = head [(ri, ci) | (ri, ci, ch) <- indexes, ch == 's']
+        (er, ec) = head [(ri, ci) | (ri, ci, ch) <- indexes, ch == 'e']
+        empty = [(ri, ci) | (ri, ci, ch) <- indexes, ch == '.' || ch == 'e']
+        solved = solve [(sr, sc, 0)] empty
+    in if head [price | (r, c, price) <- solved, r == er, c == ec] == m
+       then m
+       else -1
+
     
